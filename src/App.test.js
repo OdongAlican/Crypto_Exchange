ef46d407095a68d027d02c31d5c21ec00d15cbde
OdongAlican <sandieo.2020@gmail.com>
2021-12-02 20:35:17 +0300

    add test

--- src/App.test.js
+++ src/App.test.js
@@ -3,21 +3,6 @@ import App from './App';
 import '@testing-library/jest-dom/extend-expect';
 import renderer from 'react-test-renderer';
 
-const setup = () => {
-  const utils = render(<App />)
-  const input = utils.getByLabelText('value-input')
-  return { input, ...utils }
-}
-
-it('It should not allow letters to be inputted', () => {
-  const {input} = setup();
-  expect(input.value).toBe('1');
-  fireEvent.change(input, {target: {value: 'Random String'}})
-  expect(input.value).toBe('');
-  fireEvent.change(input, {target: {value: 12}})
-  expect(input.value).toBe("12");
-});
-
 it('It renders App component correctly', () => {
   const { getAllByTestId } = render(<App />);
   const result = getAllByTestId('appCoin');
